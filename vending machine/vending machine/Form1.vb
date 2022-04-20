f621fb1205187137ab4e29c6350becd594a3e488
kitykatt <90800597+kitykatt@users.noreply.github.com>
2022-04-19 20:24:46 -0700

    Add files via upload

--- vending machine/vending machine/Form1.vb
+++ vending machine/vending machine/Form1.vb
@@ -2,30 +2,25 @@
     Dim vend As New bank
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         vend.addNickel()
-        totallabel.Text = vend.total.ToString("c2")
-        If totallabel.Text >= 1 Then
-            canbuy.Text = "select option"
-        End If
+        linez()
     End Sub
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
         vend.addDime()
-        totallabel.Text = vend.total.ToString("c2")
-        If totallabel.Text >= 1 Then
-            canbuy.Text = "select option"
-        End If
+        linez()
     End Sub
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
         vend.addQuarter()
-        totallabel.Text = vend.total.ToString("c2")
-        If totallabel.Text >= 1 Then
-            canbuy.Text = "select option"
-        End If
+        linez()
     End Sub
     Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
         vend.adddollars()
+        linez()
+    End Sub
+    Function linez()
         totallabel.Text = vend.total.ToString("c2")
         If totallabel.Text >= 1 Then
             canbuy.Text = "select option"
         End If
-    End Sub
+    End Function
+
 End Class
